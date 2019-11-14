2264dab2fa962d34ae3c5be8c6ef804b41dee776
arogss <abcarogundade@yahoo.co.uk>
2019-11-14 15:05:30 -0500

    submission

---
+++ Assgn2/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Assgn2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assgn2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MyProfile(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("MyProfile", model);
+            }
+            
+            return View(model);
+            
+        }
+
+        public ActionResult MyProfile()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
